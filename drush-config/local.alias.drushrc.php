e067a2483268eae791450f0b74b837c1633f5dcf
Neal Vanmeert <neal@emuse.be>
2015-10-05 10:52:23 +0200

    Added ACC settings to crush config file.

--- drush-config/local.alias.drushrc.php
+++ drush-config/local.alias.drushrc.php
@@ -3,3 +3,8 @@ $aliases['local'] = array(
   'uri' => 'cegeka-behat.dev',
   'root' => '/Users/nealv/Sites/cegeka/behat-demo/web',
 );
+
+$aliases['acc'] = array(
+  'uri' => 'cegeka-behat.dev',
+  'root' => '/var/www/web',
+);
