b02646e5283f4cf01506ab20b82fbd7dc44ec724
Neal Vanmeert <neal@emuse.be>
2015-10-05 11:14:57 +0200

    Updated rush configuration for acc.

--- drushconfig/aliases.drushrc.php
+++ drushconfig/aliases.drushrc.php
@@ -1,17 +1,12 @@
 <?php
 
-$aliases['prod'] = array(
-  'uri' => 'cegeka-behat.dev',
-  'root' => '/var/www/web',
-);
-
 $aliases['local'] = array(
   'uri' => 'cegeka-behat.dev',
   'root' => '/Users/nealv/Sites/cegeka/behat-demo/web',
 );
 
 $aliases['acc'] = array(
-  'uri' => 'cegeka-behat.dev',
+  'uri' => 'cegeka-behat.cloudapp.net',
   'root' => '/var/www/web',
 );
 
