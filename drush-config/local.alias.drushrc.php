4154da68b7925b310a560e27f8e8dde21adf9df6
Neal Vanmeert <neal@emuse.be>
2015-10-05 11:02:32 +0200

    Add prod environment.

--- drush-config/local.alias.drushrc.php
+++ drush-config/local.alias.drushrc.php
@@ -1,4 +1,10 @@
 <?php
+
+$aliases['prod'] = array(
+  'uri' => 'cegeka-behat.dev',
+  'root' => '/var/www/web',
+);
+
 $aliases['local'] = array(
   'uri' => 'cegeka-behat.dev',
   'root' => '/Users/nealv/Sites/cegeka/behat-demo/web',
